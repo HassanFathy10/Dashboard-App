0c45bed2c38adfd1be4294c40a5415f36b1f0e97
Hassan Fathy <hassanfathy460@gmail.com>
2023-09-27 00:08:20 +0300

    chart.js

--- src/Component/SideBar.jsx
+++ src/Component/SideBar.jsx
@@ -1,34 +1,35 @@
 import React from 'react'
-import * as Unicons from '@iconscout/react-unicons';
-import Styles from './style.module.css';
 import { Link } from 'react-router-dom';
 import { Nav, Navbar } from 'react-bootstrap';
+import * as Unicons from '@iconscout/react-unicons';
+import Styles from './style.module.css';
+
 
 export default function SideBar() {
     return (
-        <Navbar  className='bg-white shadow'>
-                <Nav className='col-lg-2 col-md-2 col-sm-2 pt-3'>
-                    <article className='row'>
-                        <article className='d-flex min-vh-100'>
-                            <article className=' p-4 list-group'>
-                                <Nav.Link className='p-3  mb-5'><Unicons.UilUser /></Nav.Link>
-                                <article className='list-group'>
-                                    <Nav.Link className={`p-3 mb-2 ${Styles.link}`}><Link to='/'><Unicons.UilUsersAlt /></Link></Nav.Link>
-                                    <Nav.Link className={`p-3 mb-2 ${Styles.link}`}><Unicons.UilCalendarAlt /></Nav.Link>
-                                    <Nav.Link className={`p-3 mb-2 ${Styles.link}`}><Unicons.UilShareAlt /></Nav.Link>
-                                    <Nav.Link className={`p-3 mb-2 ${Styles.link}`}><Link to='/Option2'><Unicons.UilFileAlt color='fffff' /></Link></Nav.Link>
-                                    <Nav.Link className={`p-3 mb-2 ${Styles.link}`}><Unicons.UilDiaryAlt /></Nav.Link>
-                                    <Nav.Link className={`p-3 mb-2 ${Styles.link}`}><Unicons.UilHeart /></Nav.Link>
-                                    <Nav.Link className={`p-3 mb-2 ${Styles.link}`}><Unicons.UilAngleLeft /></Nav.Link>
-                                </article>
-                                <article className='mt-auto list-group my-4'>
-                                    <Nav.Link className={`p-3 mb-2 ${Styles.link}`}><Unicons.UilSetting /></Nav.Link>
-                                    <span className={`p-3 fw-bolder fs-5 ${Styles.user}`}>AS</span>
-                                </article>
+        <Navbar className='bg-white shadow'>
+            <Nav className='col-lg-2 col-md-2 col-sm-2 pt-3'>
+                <article className='row'>
+                    <article className='d-flex min-vh-100'>
+                        <article className=' p-4 list-group'>
+                            <span className='p-3  mb-5'><Unicons.UilUser /></span>
+                            <article className='list-group'>
+                                <span className={`p-3 mb-2 ${Styles.link}`}><Link to='/'><Unicons.UilUsersAlt /></Link></span>
+                                <span className={`p-3 mb-2 ${Styles.link}`}><Unicons.UilCalendarAlt /></span>
+                                <span className={`p-3 mb-2 ${Styles.link}`}><Unicons.UilShareAlt /></span>
+                                <span className={`p-3 mb-2 ${Styles.link}`}><Link to='/Option2'><Unicons.UilFileAlt color='fffff' /></Link></span>
+                                <span className={`p-3 mb-2 ${Styles.link}`}><Unicons.UilDiaryAlt /></span>
+                                <span className={`p-3 mb-2 ${Styles.link}`}><Unicons.UilHeart /></span>
+                                <span className={`p-3 mb-2 ${Styles.link}`}><Unicons.UilAngleLeft /></span>
+                            </article>
+                            <article className='mt-auto list-group my-4'>
+                                <span className={`p-3 mb-2 ${Styles.link}`}><Unicons.UilSetting /></span>
+                                <span className={`p-3 fw-bolder fs-5 ${Styles.user}`}>AS</span>
                             </article>
                         </article>
                     </article>
-                </Nav>
+                </article>
+            </Nav>
         </Navbar>
     );
 };
--- src/Pages/Option2.jsx
+++ src/Pages/Option2.jsx
@@ -1,11 +1,61 @@
 import React from 'react';
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import Dropdown from 'react-bootstrap/Dropdown';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import * as Unicons from '@iconscout/react-unicons';
 import Styles from './style.module.css';
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top'
+      ,
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Line Chart',
+    },
+  },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+const dataset1Data = [100, 200, 300, 400, 500, 600, 700];
+const dataset2Data = [50, 150, 250, 350, 450, 550, 650];
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: dataset1Data,
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: dataset2Data,
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
 export default function Option2() {
+
+
+
   return (
-    <article className='d-flex flex-wrap mx-4 position-absolute'>
+    <article className='d-flex flex-wrap mx-4 position-absolute flex-wrap'>
       {/* head section 1 */}
       <article className='col-lg-5 col-md-10 col-sm-10 pb-5'>
         <article className="mt-3 pt-5 pb-5">
@@ -56,7 +106,7 @@ export default function Option2() {
         </article>
         {/* chart */}
         <article className='mt-3'>
-          <article className='card border-0 rounded-4' style={{ width: 'rem' }}>
+          <article className='card border-0 rounded-4' style={{ width: '40rem' }}>
             <article className=' card-body d-flex flex-column pb-4'>
               <NavDropdown id="nav-dropdown-dark-example" className='fw-bold fs-6 pb-4' title="Recommended">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
@@ -69,9 +119,15 @@ export default function Option2() {
                   Separated link
                 </NavDropdown.Item>
               </NavDropdown>
+              <article style={{ width: '600px' }}>
+                <Line options={options} data={data} />
+              </article>
             </article>
           </article>
         </article>
+      </article>
+      <article className='col-lg-5'>
+        
       </article>
     </article>
   );
