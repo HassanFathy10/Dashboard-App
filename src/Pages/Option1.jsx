cc869485d54b5edaf145a6d857620a9380a6e06a
Hassan Fathy <hassanfathy460@gmail.com>
2023-09-26 15:51:05 +0300

    section 2

--- src/Pages/Option1.jsx
+++ src/Pages/Option1.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
 import Header1 from '../Component/Header1';
 import ListGroup from 'react-bootstrap/ListGroup';
 import * as Unicons from '@iconscout/react-unicons';
@@ -8,7 +9,7 @@ export default function Option1() {
     return (
         <article className={`min-vh-100 ${Styles.cover}`}>
             <Header1 />
-            <article className='d-flex'>
+            <article className='d-flex gap-5'>
                 <article className='col-lg-4'>
                     <input
                         className="form-control form-control-lg"
@@ -64,10 +65,41 @@ export default function Option1() {
                         </ListGroup.Item>
                     </ListGroup>
                 </article>
-            </article>
-            <article className='col-6'>
-                <article className='row' >
-                    
+                <article className='col-6'>
+                    <article className='row' >
+                        <article className='card'>
+                            <article className={`d-flex p-3 ${Styles.line}`}>
+                                <Form.Check className='m-2' aria-label="option 1" />
+                                <span className='text-primary fw-bold m-2'>247 candidates</span>
+                                <article className='ms-auto'>
+                                    <span className='fw-medium text-primary m-2'>Qualified</span>
+                                    <span className={Styles.span}></span>
+                                    <span className='text-black fw-medium m-2'>Task <span className={Styles.number}>25</span></span>
+                                    <span className={Styles.span}></span>
+                                    <span className='text-black fw-medium m-2'>Disqualified <span className={Styles.number}>78</span></span>
+                                </article>
+                            </article>
+                            <article className={`d-flex p-3 gap-5 ${Styles.line}`}>
+                                <article className='d-flex mt-5'>
+                                    <Form.Check className='p-2' aria-label="option 1" />
+                                    <article>
+                                        <span className={`p-2 ${Styles.user}`}>AS</span>
+                                    </article>
+                                </article>
+                                <ul className='list-unstyled lh-lg'>
+                                    <li className='fw-bold'>Aaliyah Sanderson</li>
+                                    <li className='fw-normal'>Riyadh, Saudi Arabia</li>
+                                    <li className='fw-light'>Bachelor - Cambridge University (2023 - 2023)</li>
+                                    <li className='fw-light text-primary'>#top_candidate &nbsp; &nbsp; &nbsp;  #top_candidate</li>
+                                    <li className='fw-medium text-info'>
+                                        <span className={Styles.info}>New York</span> &nbsp;
+                                        <span className={Styles.info}>Marketing</span> &nbsp;
+                                        <span className={Styles.info}>Londan</span>
+                                    </li>
+                                </ul>
+                            </article>
+                        </article>
+                    </article>
                 </article>
             </article>
         </article>
