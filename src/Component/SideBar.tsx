43b6cc6e6c354ae91bb4b00223e9a971ee47925f
Hassan Fathy <hassanfathy460@gmail.com>
2023-11-17 02:45:50 +0200

    update sidebar

---
+++ src/Component/SideBar.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Styles from './style.module.css';
+import {
+    FaUsers,
+    FaHome,
+    FaRegCalendarCheck,
+    FaShareAlt,
+    FaRegUserCircle,
+    FaRegFileAlt,
+    FaHeart
+} from "react-icons/fa";
+import { IoMdSettings } from "react-icons/io";
+
+import {
+    MenuFoldOutlined,
+    MenuUnfoldOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu, Button } from 'antd';
+const { Header, Sider } = Layout;
+
+export default function SideBar() {
+    const [collapsed, setCollapsed] = useState(false);
+    return (
+        <article>
+            <Layout className=' min-vh-100 '>
+                <Sider trigger={null} collapsible collapsed={collapsed}>
+                    <Menu
+                        theme="dark"
+                        mode="inline"
+                        defaultSelectedKeys={['1']}
+                    >
+                        <Menu.Item key="1" className='mb-5' icon={<FaRegUserCircle className='fs-4' />}>
+                            <Link to="/" className='text-decoration-none'>Hassan Fathy</Link>
+                        </Menu.Item>
+                        <Menu.Item key="2" icon={<FaHome className='fs-4' />}>
+                            <Link to="/" className='text-decoration-none'>Home</Link>
+                        </Menu.Item>
+                        <Menu.Item key="3" icon={<FaUsers className='fs-4' />}>
+                            <Link to="/Option2" className='text-decoration-none'>nav 2</Link>
+                        </Menu.Item>
+                        <Menu.Item key="4" icon={<FaRegCalendarCheck className='fs-4' />}>
+                            <Link to="/" className='text-decoration-none'>nav 3</Link>
+                        </Menu.Item>
+                        <Menu.Item key="5" icon={<FaShareAlt className='fs-4' />}>
+                            <Link to="/" className='text-decoration-none'>nav 3</Link>
+                        </Menu.Item>
+                        <Menu.Item key="6" icon={<FaRegFileAlt className='fs-4' />}>
+                            <Link to="/" className='text-decoration-none'>nav 3</Link>
+                        </Menu.Item>
+                        <Menu.Item key="7" icon={<FaHeart className='fs-4' />}>
+                            <Link to="/" className='text-decoration-none'>nav 3</Link>
+                        </Menu.Item>
+                        <Menu.Item key="8" icon={<IoMdSettings className='fs-4' />}>
+                            <Link to="/" className='text-decoration-none'>nav 3</Link>
+                        </Menu.Item>
+                        <Menu.Item key="9" icon={<FaRegUserCircle className='fs-4' />}>
+                            <Link to="/" className='text-decoration-none'>nav 3</Link>
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Layout>
+                    <Header className={`p-0 position-fixed  ${Styles.link}`}>
+                        <Button
+                            icon={collapsed ? <MenuUnfoldOutlined className='fs-5' /> : <MenuFoldOutlined className='fs-5' />}
+                            onClick={() => setCollapsed(!collapsed)}
+                            className='btn'
+                        />
+                    </Header>
+                </Layout>
+            </Layout>
+        </article>
+    );
+};
