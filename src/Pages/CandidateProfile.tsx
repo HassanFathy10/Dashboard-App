f21b5d36b128f2e4ba4fc22340883fae8dba2d68
Hassan Fathy <hassanfathy460@gmail.com>
2023-11-19 07:13:39 +0200

    Update CandidateProfile.tsx

--- src/Pages/CandidateProfile.tsx
+++ src/Pages/CandidateProfile.tsx
@@ -7,36 +7,38 @@ export default function CandidatesProfile() {
         <article className='container'>
             <article className='row'>
                 <article className='col-lg-10 card m-5 border-0 shadow'>
-                    <article>
-                        <img src='' alt='' className=' bg-black rounded-5' />
-                    </article>
-                    <article className='d-flex gap-5'>
-                        <h4>Aaliyah Sanderson</h4>
-                        <article className='mt-1'>
-                            <ul className='d-flex list-unstyled gap-5'>
-                                <li>New York</li>
-                                <li>Marketing</li>
-                                <li>London</li>
+                    <article className='d-inline-block'>
+                        <article>
+                            <img src='' alt='' className=' bg-black rounded-5' />
+                        </article>
+                        <article className='d-flex gap-5'>
+                            <h4>Aaliyah Sanderson</h4>
+                            <article className='mt-1'>
+                                <ul className='d-flex list-unstyled gap-5'>
+                                    <li>New York</li>
+                                    <li>Marketing</li>
+                                    <li>London</li>
+                                </ul>
+                            </article>
+                        </article>
+                        <article>
+                            <ul className='d-flex list-unstyled gap-2'>
+                                <li >Saudi Arabia</li>
+                                <li className={`${Styles.list}`}></li>
+                                <li>aaliyahs@gmail.com</li>
+                                <li className={`${Styles.list}`}></li>
+                                <li>+00 000 000 0000</li>
                             </ul>
                         </article>
-                        <article className='mx-auto'>
+                        <article className='col-8'>
+                            <TagCandidate />
+                        </article>
+                    </article>
+                    <article className='text-end'>
+                        <article>
                             <p>Overall Score <span>7</span></p>
                         </article>
-                    </article>
-                    <article>
-                        <ul className='d-flex list-unstyled gap-5'>
-                            <li >Saudi Arabia</li>
-                            <li className={`${Styles.list}`}></li>
-                            <li>aaliyahs@gmail.com</li>
-                            <li className={`${Styles.list}`}></li>
-                            <li>+00 000 000 0000</li>
-                        </ul>
-                    </article>
-                    <article className='d-flex'>
-                        <article className='col-8'>
-                        <TagCandidate />
-                        </article>
-                        <article className=' mx-auto'>
+                        <article>
                             <p className='text-black-50'>Applied on 12 June 2023</p>
                         </article>
                     </article>
