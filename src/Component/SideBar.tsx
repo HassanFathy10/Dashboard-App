abcfa47de6fbb01f6f543a0e5f3248d36be13dbc
Hassan Fathy <118758238+HassanFathy10@users.noreply.github.com>
2024-02-12 21:42:33 +0200

    Update SideBar.tsx

--- src/Component/SideBar.tsx
+++ src/Component/SideBar.tsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 import Styles from './style.module.css';
+import { Nav, Navbar } from 'react-bootstrap';
 import {
     FaUsers,
     FaHome,
